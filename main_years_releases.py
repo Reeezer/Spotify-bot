199a3453270185eb7e5d04f2af855779b3b4345e
Reeezer <leonmuller@hotmail.fr>
2022-08-28 18:51:48 +0200

    Changes of the output

--- main_years_releases.py
+++ main_years_releases.py
@@ -2,42 +2,35 @@ from spotifyclient import SpotifyClient
 import utils.credentials as credentials
 from datetime import datetime, timedelta
 
+from models.track import Track
+
+
+def create_playlist_for_last_years(years: int, liked_tracks: list[Track]):
+    playlist = spotify_client.create_playlist(f"🤖 Last {years} years", f"My liked tracks from the last {years} years")
+    last_years = datetime.today() - timedelta(days=365*years)
+    tracks = [track for track in liked_tracks if track.release_date >= last_years]
+    spotify_client.populate_playlist(playlist, tracks)
+
 
 spotify_client = SpotifyClient(credentials.SPOTIFY_AUTHORIZATION_TOKEN, credentials.SPOTIFY_CLIENT_ID)
 
 
 # Display all the liked tracks of the user
+print("Gathering liked tracks... ", end="", flush=True)
 liked_tracks = spotify_client.get_all_liked_tracks()
-print(f"\nHere are the liked tracks of the user:")
-for index, track in enumerate(liked_tracks):
-    print(f"{index+1} - {track}")
-
+print("Done")
 
 # Create a playlist for the last 1 year releases
-playlist = spotify_client.create_playlist("🤖 Last 1 year", "My liked tracks from the last 1 year")
-
-last_year = datetime.today() - timedelta(days=365)
-tracks = [track for track in liked_tracks if track.release_date >= last_year]
-
-print(f"\nAdding {len(tracks)} tracks from the last 1 year to the playlist...")
-spotify_client.populate_playlist(playlist, tracks)
-
+print("Creating a playlist for the last 1 year releases... ", end="", flush=True)
+create_playlist_for_last_years(1, liked_tracks)
+print("Done")
 
 # Create a playlist for the last 3 years releases
-playlist = spotify_client.create_playlist("🤖 Last 3 years", "My liked tracks from the last 3 years")
-
-last_year = datetime.today() - timedelta(days=365*3)
-tracks = [track for track in liked_tracks if track.release_date >= last_year]
-
-print(f"\nAdding {len(tracks)} tracks from the last 3 years to the playlist...")
-spotify_client.populate_playlist(playlist, tracks)
-
+print("Creating a playlist for the last 3 years releases... ", end="", flush=True)
+create_playlist_for_last_years(3, liked_tracks)
+print("Done")
 
 # Create a playlist for the last 5 years releases
-playlist = spotify_client.create_playlist("🤖 Last 5 years", "My liked tracks from the last 5 years")
-
-last_year = datetime.today() - timedelta(days=365*5)
-tracks = [track for track in liked_tracks if track.release_date >= last_year]
-
-print(f"\nAdding {len(tracks)} tracks from the last 5 years to the playlist...")
-spotify_client.populate_playlist(playlist, tracks)
+print("Creating a playlist for the last 5 years releases... ", end="", flush=True)
+create_playlist_for_last_years(5, liked_tracks)
+print("Done")
